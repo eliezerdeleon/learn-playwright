08f1076a13f669f2eb3e9bb2722667934967da85
Eliezer <eliezer.deleon1@hotmail.com>
2024-12-27 12:12:31 -0400

    added some locators

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -6,5 +6,7 @@ const context = await browser.newContext();
 const page = await context.newPage();
 
 await page.goto("https://ecommerce-playground.lambdatest.io/")
+await page.hover("//a[@data-toggle='dropdown']//span[contains(.,'My account')]")
+await page.click("text=Login")
 
 })
