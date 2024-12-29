eda260f6a32ab71b37c73fe6094067a7041c8ac9
Eliezer <eliezer.deleon1@hotmail.com>
2024-12-29 11:54:23 -0400

    added login credentials

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -10,8 +10,13 @@ const page = await context.newPage();
 await page.goto("https://ecommerce-playground.lambdatest.io/")
 await page.hover("//a[@data-toggle='dropdown']//span[contains(.,'My account')]")
 //await page.click("text=Login")
-
 await page.click("'Login'")
+await page.waitForTimeout(5000);
+
+await page.fill("input[name='email']", "koushik350@gmail.com")
+await page.fill("input[name='password']", "Pass123")
+
+await page.click("input[value='Login']");
 
 await page.waitForTimeout(5000);
 
