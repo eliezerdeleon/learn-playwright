821b96fcfd584fe0d8826fb1f8f8b54b4f26216e
Eliezer <eliezer.deleon1@hotmail.com>
2024-12-30 11:42:46 -0400

    tested recorded action script

---
+++ tests/recorded.test.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://ecommerce-playground.lambdatest.io/');
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByPlaceholder('E-Mail Address').click();
+  await page.getByPlaceholder('E-Mail Address').fill('eliezer.deleon1@hotmail.com');
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill('Ed123456$');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('link', { name: ' Edit your account' }).click();
+  await page.getByPlaceholder('First Name').click();
+  await page.getByPlaceholder('First Name').fill('EliezerNatanael');
+  await page.getByRole('button', { name: 'Continue' }).click();
+  await page.getByRole('link', { name: 'Logout', exact: true }).click();
+});
