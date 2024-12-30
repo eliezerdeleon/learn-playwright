a60659ef66399ab9768d0cd36a3ff964c4d7fdc9
Eliezer <eliezer.deleon1@hotmail.com>
2024-12-30 12:14:22 -0400

    added headless property

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,11 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
-    testMatch:["tests/login.test.ts"]
+    //testMatch:["tests/login.test.ts"]
+    testMatch: ["tests/recorded.test.ts"],
+    use: {
+        headless: false
+    }
 };
 
 export default config;
