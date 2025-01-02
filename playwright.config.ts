9c32e0256b41dd2fd8d65654d2e6b11a3d4ba8b8
Eliezer <eliezer.deleon1@hotmail.com>
2025-01-02 17:25:48 -0400

    added playwright report code

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,15 @@ const config: PlaywrightTestConfig = {
     testMatch: ["tests/recorded.test.ts"],
     use: {
         headless: false
-    }
+    },
+    
+reporter: [["dot"],["json",{
+    outputFile: "jsonReports/jsonReport.json"
+
+}],["html",{
+    open: "never"
+
+}]]
 };
 
 export default config;
