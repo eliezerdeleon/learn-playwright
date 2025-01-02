396040fe4cdaa1a47dd892e1c1d960a4c7913f41
Eliezer <eliezer.deleon1@hotmail.com>
2025-01-02 14:09:35 -0400

    added the mouse hover locator

--- tests/recorded.test.ts
+++ tests/recorded.test.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://ecommerce-playground.lambdatest.io/');
+  await page.hover("//a[@data-toggle='dropdown']//span[contains(.,'My account')]")
   await page.getByRole('link', { name: 'Login' }).click();
   await page.getByPlaceholder('E-Mail Address').click();
   await page.getByPlaceholder('E-Mail Address').fill('eliezer.deleon1@hotmail.com');
